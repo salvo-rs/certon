dd325852569998f21c226a1ba8edb3052c8932d8
Chris Young <chris@acroidea.com>
2026-09-07 15:36:20 +0800

    Initialize the provider in the top-level manage entry point

--- src/lib.rs
+++ src/lib.rs
@@ -130,6 +130,7 @@ pub use zerossl_issuer::{ZeroSslApiIssuer, ZeroSslIssuer};
 /// }
 /// ```
 pub async fn manage(domains: &[String]) -> Result<rustls::ServerConfig> {
+    install_default_crypto_provider();
     let storage: Arc<dyn Storage> = Arc::new(FileStorage::default());
     let config = Config::builder().storage(storage).build();
     config.manage_sync(domains).await?;
